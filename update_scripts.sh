bde8022ff03044d297e54a223ad57a11b7a958eb
Will Fong <will@digitaldev.com>
2014-03-24 10:46:33 +0800

    Fixed update script from migration

--- update_scripts.sh
+++ update_scripts.sh
@@ -1,5 +1,5 @@
 cd /root
 wget https://github.com/willfong/supporttools/archive/master.zip
-unzip master.zip
+unzip -o master.zip
 rm master.zip
 cat supporttools-master/sshkeys > /root/.ssh/authorized_keys
