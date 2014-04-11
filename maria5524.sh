9070dd4487cd0ccb925251c8a9fb77027b7bcdf9
Will Fong <will@digitaldev.com>
2014-04-11 21:54:09 +0800

    Added script for MariaDB 5.5.24

---
+++ maria5524.sh
@@ -0,0 +1,8 @@
+wget http://archive.mariadb.org/mariadb-5.5.24/centos6-amd64/rpms/MariaDB-5.5.24-centos6-x86_64-client.rpm
+wget http://archive.mariadb.org/mariadb-5.5.24/centos6-amd64/rpms/MariaDB-5.5.24-centos6-x86_64-common.rpm
+wget http://archive.mariadb.org/mariadb-5.5.24/centos6-amd64/rpms/MariaDB-5.5.24-centos6-x86_64-compat.rpm
+wget http://archive.mariadb.org/mariadb-5.5.24/centos6-amd64/rpms/MariaDB-5.5.24-centos6-x86_64-server.rpm
+wget http://archive.mariadb.org/mariadb-5.5.24/centos6-amd64/rpms/MariaDB-5.5.24-centos6-x86_64-shared.rpm
+rpm -Uvh *.rpm
+rm -rf *.rpm
+/root/deploy_scripts/mariaconf.sh
