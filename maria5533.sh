fe6a014e776a4efe205fe5fd6fefd88766103b12
Will Fong <will@digitaldev.com>
2014-05-01 18:29:46 +0900

    Out with broken 5.5.33, in with 5.5.35

--- maria5533.sh
+++
@@ -1,8 +0,0 @@
-wget http://`cat /webserver.txt`/mysql/MariaDB-5.5.33a-centos6-x86_64-client.rpm
-wget http://`cat /webserver.txt`/mysql/MariaDB-5.5.33a-centos6-x86_64-common.rpm
-wget http://`cat /webserver.txt`/mysql/MariaDB-5.5.33a-centos6-x86_64-compat.rpm
-wget http://`cat /webserver.txt`/mysql/MariaDB-5.5.33a-centos6-x86_64-server.rpm
-wget http://`cat /webserver.txt`/mysql/MariaDB-5.5.33a-centos6-x86_64-shared.rpm
-rpm -Uvh *.rpm
-rm -rf *.rpm
-/root/deploy_scripts/mariaconf.sh
