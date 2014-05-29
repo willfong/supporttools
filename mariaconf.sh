f671f141f97386acbfe81f05d3b3aeac5b772113
Will Fong <will@digitaldev.com>
2014-05-29 09:55:37 +0800

    conf cleanup

--- mariaconf.sh
+++ mariaconf.sh
@@ -1,5 +1,7 @@
 cat <<EOF > /etc/my.cnf.d/server.cnf
 [mysqld]
+log_error=/var/lib/mysql/mysql.err
+
 innodb_buffer_pool_size=2G
 innodb_log_file_size=1G
 sync_binlog=0
@@ -8,6 +10,5 @@ skip-innodb_doublewrite
 innodb_old_blocks_time=1000
 innodb_purge_threads=1
 innodb_file_per_table=1
-
 performance_schema=off
 EOF
