2139a27dc4842c0da18c66e98ea0102a844d2f8c
Will Fong <will@digitaldev.com>
2014-05-29 11:14:38 +0800

    MHA-manager. Needs to test

---
+++ mha-manager.sh
@@ -0,0 +1,3 @@
+yum -y install perl-DBD-MySQL perl-Config-Tiny perl-MIME-Lite perl-Email-Date-Format perl-Time-HiRes
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/perl-Log-Dispatch-2.27-1.el6.noarch.rpm http://dl.fedoraproject.org/pub/epel/6/x86_64/perl-Parallel-ForkManager-0.7.9-1.el6.noarch.rpm http://dl.fedoraproject.org/pub/epel/6/x86_64/perl-Mail-Sender-0.8.16-3.el6.noarch.rpm http://dl.fedoraproject.org/pub/epel/6/x86_64/perl-Mail-Sendmail-0.79-12.el6.noarch.rpm https://googledrive.com/host/0B1lu97m8-haWeHdGWXp0YVVUSlk/mha4mysql-manager-0.56-0.el6.noarch.rpm https://googledrive.com/host/0B1lu97m8-haWeHdGWXp0YVVUSlk/mha4mysql-node-0.56-0.el6.noarch.rpm
+
