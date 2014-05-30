b83f8f3ca1c19c3c98b5ed33470163819ce3227d
Will Fong <will@digitaldev.com>
2014-05-30 08:45:47 +0800

    Removing scripts that was used for esxi

--- mysql5531.sh
+++
@@ -1,6 +0,0 @@
-wget http://`cat /webserver.txt`/mysql5531.el6.x86_64.tar
-tar xf mysql5531.el6.x86_64.tar
-rpm -Uvh *.rpm
-rm mysql5531.el6.x86_64.tar
-rm -rf *.rpm
-service mysql start
--- mysql5614.sh
+++
@@ -1,8 +0,0 @@
-wget http://`cat /webserver.txt`/mysql/MySQL-client-5.6.14-1.el6.x86_64.rpm
-wget http://`cat /webserver.txt`/mysql/MySQL-server-5.6.14-1.el6.x86_64.rpm
-wget http://`cat /webserver.txt`/mysql/MySQL-shared-5.6.14-1.el6.x86_64.rpm
-wget http://`cat /webserver.txt`/mysql/MySQL-shared-compat-5.6.14-1.el6.x86_64.rpm
-rpm -Uvh *.rpm
-rm -rf *.rpm
-service mysql start
-./reset_mysql_password.sh
