f27870922c921a90d2f9c5ad687779e13a337eca
Will Fong <will@digitaldev.com>
2014-06-05 13:27:22 +0800

    using archive section now

--- install/mysql5536.sh
+++ install/mysql5536.sh
@@ -1,4 +1,4 @@
-wget http://dev.mysql.com/get/Downloads/MySQL-5.5/MySQL-5.5.36-1.el6.x86_64.rpm-bundle.tar 
+wget http://downloads.mysql.com/archives/get/file/MySQL-5.5.36-1.linux2.6.x86_64.rpm-bundle.tar
 tar xf MySQL-5.5.36-1.el6.x86_64.rpm-bundle.tar 
 rpm -Uvh MySQL-client-5.5.36-1.el6.x86_64.rpm MySQL-server-5.5.36-1.el6.x86_64.rpm MySQL-shared-5.5.36-1.el6.x86_64.rpm MySQL-shared-compat-5.5.36-1.el6.x86_64.rpm
 rm -rf MySQL-5.5.36-1.el6.x86_64.rpm-bundle.tar  
