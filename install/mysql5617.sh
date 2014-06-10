0c98889848a863a6040f9f575a5146245254a647
Will Fong <will@digitaldev.com>
2014-06-10 12:27:08 +0800

    Updating with new locations and devel packages

--- install/mysql5617.sh
+++ install/mysql5617.sh
@@ -1,6 +1,6 @@
 wget http://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-5.6.17-1.el6.x86_64.rpm-bundle.tar
 tar xvf MySQL-*-bundle.tar
-rpm -Uvh MySQL-shared-*.rpm MySQL-server-*.rpm MySQL-client-*.rpm MySQL-shared-compat-*.rpm
+rpm -Uvh MySQL-devel-*.rpm MySQL-shared-*.rpm MySQL-server-*.rpm MySQL-client-*.rpm MySQL-shared-compat-*.rpm
 rm -rf *.rpm
 rm -rf MySQL-*-bundle.tar
 service mysql start
