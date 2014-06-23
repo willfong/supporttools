b9ee8758fe96347a34f756e8e44453580bb72b21
Will Fong <will@digitaldev.com>
2014-06-23 15:40:16 +0800

    adding in devel packages for sysbench

--- install/mysql5536.sh
+++ install/mysql5536.sh
@@ -1,5 +1,5 @@
 wget http://downloads.mysql.com/archives/get/file/MySQL-5.5.36-1.linux2.6.x86_64.rpm-bundle.tar
 tar xf MySQL-5.5.36-1.linux2.6.x86_64.rpm-bundle.tar
-rpm -Uvh MySQL-client-5.5.36-1.linux2.6.x86_64.rpm MySQL-server-5.5.36-1.linux2.6.x86_64.rpm MySQL-shared-5.5.36-1.linux2.6.x86_64.rpm MySQL-shared-compat-5.5.36-1.linux2.6.x86_64.rpm
+rpm -Uvh MySQL-devel-*.rpm MySQL-shared-*.rpm MySQL-server-*.rpm MySQL-client-*.rpm MySQL-shared-compat-*.rpm
 rm -rf MySQL-5.5.36-1.linux2.6.x86_64.rpm-bundle.tar
 rm -rf *.rpm
