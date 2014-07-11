1067f2990a4fdcae0a3de07c016cb2ac287de330
Will Fong <will@digitaldev.com>
2014-07-11 11:57:08 +0800

    Adding Percona's TPCC toolkit

---
+++ install/tpcc-percona.sh
@@ -0,0 +1,15 @@
+yum -y install bzr
+yum -y install mysql-devel
+bzr branch lp:~percona-dev/perconatools/tpcc-mysql
+cd tpcc-mysql/src
+make
+cd ..
+
+echo "
+
+Reference:
+
+  http://www.mysqlperformanceblog.com/2013/07/01/tpcc-mysql-simple-usage-steps-and-how-to-build-graphs-with-gnuplot/
+  http://bazaar.launchpad.net/~percona-dev/perconatools/tpcc-mysql/view/head:/README
+
+"
