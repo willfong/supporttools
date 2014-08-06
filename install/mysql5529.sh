2c5dc4277902a71dc9877676359f75198e22090b
Will Fong <will@digitaldev.com>
2014-08-06 09:09:26 +0800

    Needs libaio

--- install/mysql5529.sh
+++ install/mysql5529.sh
@@ -1,3 +1,3 @@
+yum -y install libaio
 rpm -Uvh http://downloads.mysql.com/archives/get/file/MySQL-shared-5.5.29-2.el6.x86_64.rpm http://downloads.mysql.com/archives/get/file/MySQL-shared-compat-5.5.29-2.el6.x86_64.rpm http://downloads.mysql.com/archives/get/file/MySQL-devel-5.5.29-2.el6.x86_64.rpm http://downloads.mysql.com/archives/get/file/MySQL-server-5.5.29-2.el6.x86_64.rpm http://downloads.mysql.com/archives/get/file/MySQL-client-5.5.29-2.el6.x86_64.rpm 
-
 rm -rf *.rpm
