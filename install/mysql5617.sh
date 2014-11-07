da9e3900c5ca3d2dc3273301f1a82b82ce0c655b
Will Fong <will@digitaldev.com>
2014-11-07 10:54:33 +0800

    Making sure libaio is there

--- install/mysql5617.sh
+++ install/mysql5617.sh
@@ -1,7 +1,7 @@
+yum -y install libaio
 wget http://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-5.6.17-1.el6.x86_64.rpm-bundle.tar
 tar xvf MySQL-*-bundle.tar
 rpm -Uvh MySQL-devel-*.rpm MySQL-shared-*.rpm MySQL-server-*.rpm MySQL-client-*.rpm MySQL-shared-compat-*.rpm
 rm -rf *.rpm
 rm -rf MySQL-*-bundle.tar
-service mysql start
 echo "Remember to run: reset_mysql_password.sh"
