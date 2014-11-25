0791dfb8cb257f140bdf043f88bb1536e37c4258
Will Fong <will@digitaldev.com>
2014-11-25 09:51:34 +0800

    Libaio

--- install/mysql5615.sh
+++ install/mysql5615.sh
@@ -1,3 +1,4 @@
+yum -y install libaio
 wget http://downloads.mysql.com/archives/get/file/MySQL-5.6.15-1.el6.x86_64.rpm-bundle.tar
 tar xvf MySQL-*.rpm-bundle.tar
 rpm -Uvh MySQL-shared-*.rpm MySQL-server-*.rpm MySQL-client-*.rpm MySQL-devel-*.rpm
