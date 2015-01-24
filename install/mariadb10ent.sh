9a66d49f703dd0629c86fa2849f95c1eae7ea10e
Will Fong <will@digitaldev.com>
2015-01-24 09:34:18 +0100

    MariaDB Ent

--- install/mariadb10ent.sh
+++ install/mariadb10ent.sh
@@ -1,10 +1,8 @@
 cat <<EOF > /etc/yum.repos.d/MariaDB10.repo
-# MariaDB 10.0 CentOS repository list - created 2014-03-24 03:15 UTC
-# http://mariadb.org/mariadb/repositories/
 [mariadb]
 name = MariaDB
-baseurl = http://yum.mariadb.org/10.0/centos6-amd64
-gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
+baseurl = https://portaluser:testing@code.mariadb.com/mariadb-enterprise/10.0/yum/rhel6-amd64
+gpgkey= https://downloads.mariadb.com/files/MariaDB/RPM-GPG-KEY-MariaDB-Ent
 gpgcheck=1
 EOF
 
