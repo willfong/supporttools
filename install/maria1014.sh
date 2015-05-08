276468371d3dc11e34bef4edf4148e63ebdb5e49
Will Fong <will@digitaldev.com>
2015-05-08 16:02:35 +0800

    Added 10.1.4

---
+++ install/maria1014.sh
@@ -0,0 +1,11 @@
+wget http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.4/yum/centos6-amd64/rpms/MariaDB-10.1.4-centos6-x86_64-client.rpm
+wget http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.4/yum/centos6-amd64/rpms/MariaDB-10.1.4-centos6-x86_64-common.rpm
+wget http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.4/yum/centos6-amd64/rpms/MariaDB-10.1.4-centos6-x86_64-compat.rpm
+wget http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.4/yum/centos6-amd64/rpms/MariaDB-10.1.4-centos6-x86_64-devel.rpm
+wget http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.4/yum/centos6-amd64/rpms/MariaDB-10.1.4-centos6-x86_64-server.rpm
+wget http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.4/yum/centos6-amd64/rpms/MariaDB-10.1.4-centos6-x86_64-shared.rpm
+wget http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.4/yum/centos6-amd64/rpms/jemalloc-3.6.0-1.el6.x86_64.rpm
+
+yum -y install perl-DBI
+yum -y install *.rpm
+rm -rf *.rpm
