c14a6f93fcad6fbb47294f199210bed6ee388877
Will Fong <will@digitaldev.com>
2015-05-19 10:52:48 +0800

    Adjusted mem req

--- install/mysqlconf.sh
+++ install/mysqlconf.sh
@@ -2,8 +2,8 @@ cat <<EOF > /etc/my.cnf
 [mysqld]
 log_error=/var/lib/mysql/mysql.err
 
-innodb_buffer_pool_size=2G
-innodb_log_file_size=1G
+innodb_buffer_pool_size=256M
+innodb_log_file_size=256M
 sync_binlog=0
 innodb_flush_log_at_trx_commit=2
 skip-innodb_doublewrite
@@ -13,3 +13,4 @@ innodb_file_per_table=1
 
 performance_schema=off
 EOF
+
