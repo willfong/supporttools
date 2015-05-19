7bef1ebf122ab70ae9e85a77a70975f11c0be91c
Will Fong <will@digitaldev.com>
2015-05-19 13:55:04 +0800

    MySQL 5.5.15 binary

---
+++ install/mysql5515.sh
@@ -0,0 +1,18 @@
+yum -y install libaio
+groupadd mysql
+useradd -r -g mysql mysql
+cd /usr/local
+wget http://downloads.mysql.com/archives/get/file/mysql-5.5.15-linux2.6-x86_64.tar.gz
+tar xvzf mysql*.tar.gz
+ln -s mysql-5.5.15-linux2.6-x86_64 mysql
+cd mysql
+chown -R mysql .
+chgrp -R mysql .
+rm -rf /var/lib/mysql
+mkdir /var/lib/mysql
+scripts/mysql_install_db --user=mysql --datadir=/var/lib/mysql
+chown -R root .
+chown -R mysql /var/lib/mysql
+
+
+
