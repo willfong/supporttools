aa888a80574065dfb07a78aaec0472abc9822e1d
Will Fong <will@digitaldev.com>
2015-06-03 09:52:44 +0800

    Adding MySQL 5.6.22

---
+++ install/mysql5622.sh
@@ -0,0 +1,7 @@
+yum -y install libaio
+wget http://downloads.mysql.com/archives/get/file/MySQL-5.6.22-1.el6.x86_64.rpm-bundle.tar
+tar xvf MySQL-*-bundle.tar
+rpm -Uvh MySQL-devel-*.rpm MySQL-shared-*.rpm MySQL-server-*.rpm MySQL-client-*.rpm MySQL-shared-compat-*.rpm
+rm -rf *.rpm
+rm -rf MySQL-*-bundle.tar
+echo "Remember to run: reset_mysql_password.sh"
