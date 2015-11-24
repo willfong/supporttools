6c71b1ba4d28a1eaf0695f646a1e7e20aadb7fc5
Will Fong <will@digitaldev.com>
2015-11-24 12:24:33 +0800

    Adding EL7 sysbench

---
+++ install/el7/sysbench.sh
@@ -0,0 +1,19 @@
+yum -y group install "Development Tools"
+yum -y install bzr zlib-devel openssl-devel
+bzr branch lp:sysbench sysbench-trunk
+cd sysbench-trunk
+./autogen.sh
+./configure
+make
+
+cd
+
+echo "
+
+
+
+Sample Sysbench Command: 
+
+sysbench-trunk/sysbench/sysbench --test=sysbench-trunk/sysbench/tests/db/oltp.lua --mysql-host=127.0.0.1 --mysql-port=3306 --mysql-user=root --mysql-password= --mysql-db=test --mysql-table-engine=innodb --mysql-ignore-duplicates=on --num-threads=32 --oltp-read-only=off --oltp-dist-type=uniform --oltp-skip-trx=off --init-rng=on --oltp-test-mode=complex --max-requests=0 --report-interval=5 --oltp-table-size=1000000 --oltp-tables-count=4 --max-time=600 {run|prepare|cleanup}"
+
+
