7f5552bac7b7f8775dd8fb04f615f05790fee1e3
Will Fong <will@digitaldev.com>
2015-12-15 13:31:04 +0800

    updated mysql repo

--- install/mysql_repo.sh
+++
@@ -1,1 +0,0 @@
-rpm -Uvh https://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm
---
+++ install/mysqlrepo.sh
@@ -0,0 +1,3 @@
+rpm -Uvh https://dev.mysql.com/get/mysql57-community-release-el6-7.noarch.rpm
+
+echo "Edit /etc/yum.repos.d/mysql-community.repo to enable other GA versions"
