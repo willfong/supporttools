32d3cfb8eac7cb5e50320eb738ea4ebe88d49dfd
Will Fong <will@digitaldev.com>
2015-12-28 12:33:54 +0800

    Update Enterprise download

--- install/mariadb10ent.sh
+++ install/mariadb10ent.sh
@@ -1,9 +1,3 @@
-cat <<EOF > /etc/yum.repos.d/MariaDB10.repo
-[mariadb]
-name = MariaDB
-baseurl = https://portaluser:testing@code.mariadb.com/mariadb-enterprise/10.0/yum/rhel6-amd64
-gpgkey= https://downloads.mariadb.com/files/MariaDB/RPM-GPG-KEY-MariaDB-Ent
-gpgcheck=1
-EOF
+rpm -i http://downloads.mariadb.com/enterprise/he05-e19h/generate/10.0/mariadb-enterprise-repository.rpm
 echo "Install socat for Galera first"
 echo "yum install MariaDB-Galera-server MariaDB-client"
