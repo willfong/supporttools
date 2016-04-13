f2ec18fc11a4b6f9684a813771cb1d310124bf44
Will Fong <will@digitaldev.com>
2016-04-13 17:51:39 +0800

    Maria 10.1.3

--- install/maria1013.sh
+++ install/maria1013.sh
@@ -1,11 +1,22 @@
-wget http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.3/yum/centos6-amd64/rpms/MariaDB-10.1.3-centos6-x86_64-client.rpm
-wget http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.3/yum/centos6-amd64/rpms/MariaDB-10.1.3-centos6-x86_64-common.rpm
-wget http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.3/yum/centos6-amd64/rpms/MariaDB-10.1.3-centos6-x86_64-compat.rpm
-wget http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.3/yum/centos6-amd64/rpms/MariaDB-10.1.3-centos6-x86_64-devel.rpm
-wget http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.3/yum/centos6-amd64/rpms/MariaDB-10.1.3-centos6-x86_64-server.rpm
-wget http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.3/yum/centos6-amd64/rpms/MariaDB-10.1.3-centos6-x86_64-shared.rpm
-wget http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.3/yum/centos6-amd64/rpms/jemalloc-3.6.0-1.el6.x86_64.rpm
+URL=http://downloads.mariadb.com/files/MariaDB/mariadb-10.1.3/bintar-linux-x86_64/mariadb-10.1.3-linux-x86_64.tar.gz
 
-yum -y install perl-DBI
-yum -y install *.rpm
-rm -rf *.rpm
+yum -y install libaio
+rpm -Uvh http://downloads.mariadb.com/files/MariaDB/mariadb-10.1.8/yum/centos6-amd64/rpms/jemalloc-3.6.0-1.el6.x86_64.rpm
+groupadd mysql
+useradd -r -g mysql mysql
+cd /usr/local
+wget $URL
+tar xvzf m*.tar.gz
+ls -d m*/ | xargs -I dirname ln -s dirname mysql
+cd mysql
+chown -R mysql .
+chgrp -R mysql .
+rm -rf /var/lib/mysql
+mkdir /var/lib/mysql
+scripts/mysql_install_db --user=mysql --datadir=/var/lib/mysql
+chown -R root .
+chown -R mysql /var/lib/mysql
+
+echo "
+Start with /usr/local/mysql/bin/mysqld_safe --user=mysql &
+"
