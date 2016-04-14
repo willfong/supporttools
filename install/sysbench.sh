e51af481215705b514e86d6eee5d9d89ae3f5a94
Will Fong <will@digitaldev.com>
2016-04-14 15:01:57 +0800

    Updating to use git and install less packages

--- install/sysbench.sh
+++ install/sysbench.sh
@@ -1,7 +1,6 @@
-yum -y groupinstall "Development Tools"
-yum -y install bzr zlib-devel openssl-devel
-bzr branch lp:sysbench sysbench-trunk
-cd sysbench-trunk
+yum -y install mysql-devel git automake libtool zlib-devel openssl-devel
+git clone https://github.com/akopytov/sysbench.git
+cd sysbench
 ./autogen.sh
 ./configure
 make
@@ -11,13 +10,10 @@ cd
 echo "
 
 
+Sample Sysbench Command:
 
-Sample Sysbench Command: 
-
-sysbench-trunk/sysbench/sysbench --test=sysbench-trunk/sysbench/tests/db/oltp.lua --mysql-host=127.0.0.1 --mysql-port=3306 --mysql-user=root --mysql-password= --mysql-db=test --mysql-table-engine=innodb --mysql-ignore-duplicates=on --num-threads=32 --oltp-read-only=off --oltp-dist-type=uniform --oltp-skip-trx=off --init-rng=on --oltp-test-mode=complex --max-requests=0 --report-interval=5 --oltp-table-size=1000000 --oltp-tables-count=4 --max-time=600 {run|prepare|cleanup}
-
+sysbench/sysbench/sysbench --test=sysbench/sysbench/tests/db/oltp.lua --mysql-host=127.0.0.1 --mysql-port=3306 --mysql-user=root --mysql-password= --mysql-db=test --mysql-table-engine=innodb --mysql-ignore-duplicates=on --num-threads=32 --oltp-read-only=off --oltp-dist-type=uniform --oltp-skip-trx=off --init-rng=on --oltp-test-mode=complex --max-requests=0 --report-interval=5 --oltp-table-size=1000000 --oltp-tables-count=4 --max-time=600 {run|prepare|cleanup}
 
 5M Rows = 1.2G
 "
-
 
