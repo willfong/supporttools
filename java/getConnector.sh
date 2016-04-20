12fb8ec5bd5a026fe2bec3615cf32023e3cac2b2
Will Fong <will@digitaldev.com>
2016-04-20 16:34:14 +0800

    Adding support for MySQL Connector J

--- java/getConnector.sh
+++ java/getConnector.sh
@@ -1,1 +1,5 @@
 wget https://downloads.mariadb.com/enterprise/he05-e19h/connectors/java/connector-java-1.4.2/mariadb-java-client-1.4.2.jar
+wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.38.tar.gz
+tar xzvf mysql-connector-java-5.1.38.tar.gz mysql-connector-java-5.1.38/mysql-connector-java-5.1.38-bin.jar
+mv mysql-connector-java-5.1.38/mysql-connector-java-5.1.38-bin.jar .
+rm -rf mysql-connector-java-5.1.38/ mysql-connector-java-5.1.38.tar.gz
