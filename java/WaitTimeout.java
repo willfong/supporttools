f8c7f79cb3900764ae40d2dc800804a64dfc904d
Will Fong <will@digitaldev.com>
2016-04-20 11:40:27 +0800

    Added ability to specify waittimeout and sleep settings, and loop queries

--- java/WaitTimeout.java
+++ java/WaitTimeout.java
@@ -5,17 +5,35 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.PreparedStatement;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class WaitTimeout
 {
   public static void main(String[] args)
     throws InterruptedException {
 
-    System.out.println("Hello, Database!");
+    if (args.length == 2) {
+      int waitTimeout;
+      int sleepTime;
+      try {
+        waitTimeout = Integer.parseInt(args[0]);
+        sleepTime = Integer.parseInt(args[1]);
+      } catch (NumberFormatException e) {
+        System.err.println("Must specify numeric values.");
+        System.exit(1);
+      }
+    } else {
+      System.err.println("Must specify waitTimeout and sleepTime integers");
+      System.exit(1);
+    }
+
+    System.out.println("Java WaitTimeout Test");
 
     try {
       System.out.println("Loading driver...");
-      Class.forName("org.mariadb.jdbc.Driver");
-      //Class.forName("com.mysql.jdbc.Driver");
+      //Class.forName("org.mariadb.jdbc.Driver");
+      Class.forName("com.mysql.jdbc.Driver");
     } catch (Exception ex) {
       System.out.println("[ERROR] Error with driver: \n" + ex.getMessage());
     }
@@ -24,8 +42,8 @@ public class WaitTimeout
 
     try {
       System.out.println("Connecting to database...");
-      conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:4306/test?user=w");
-      //conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:4306/test?user=w");
+      //conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:4306/test?user=w");
+      conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:4306/test?user=w");
     } catch (SQLException ex) {
       System.out.println("Error connecting to database...");
       // handle any errors
@@ -42,17 +60,20 @@ public class WaitTimeout
       System.out.println("Starting queries...");
       stmt = conn.createStatement();
 
-      System.out.println("Set wait timeout for 10 seconds...");
-      rs = stmt.executeQuery("SET SESSION wait_timeout=10");
+      System.out.println("Set wait timeout for " + args[0] + " seconds...");
+      rs = stmt.executeQuery("SET SESSION wait_timeout=" + args[0]);
 
-      System.out.println("First select...");
-      rs = stmt.executeQuery("SELECT COUNT(*) FROM mysql.user");
 
-      System.out.println("Sleep for 20 seconds...");
-      Thread.sleep(20000);
+      String ts;
 
-      System.out.println("Second select...");
-      rs = stmt.executeQuery("SELECT COUNT(*) FROM mysql.user");
+      for (int i=0; i<10; i++) {
+        java.util.Date date= new java.util.Date();
+	ts = new Timestamp(date.getTime()).toString();
+        System.out.println("[" + ts + "] Running query: " + i);
+        rs = stmt.executeQuery("SELECT COUNT(*) FROM mysql.user");
+        System.out.println("Sleep for " + args[1] + " seconds...");
+        Thread.sleep(Integer.parseInt(args[1])*1000);
+      }
 
       System.out.println("All done!");
 
