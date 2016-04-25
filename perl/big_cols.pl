f360678786efc55370445322ea2946f694faa2e0
Will Fong <w@r10e.xyz>
2016-04-25 18:35:33 +0800

    Changed script to be compatible with sysbench

--- perl/big_cols.pl
+++ perl/big_cols.pl
@@ -12,44 +12,23 @@ if ($num_args != 1) {
 my $num_of_rcpt = $ARGV[0];
 
 print STDERR "\n\nGeneral Usage:\n\n";
-print STDERR "CREATE TABLE mytable ( a INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, b INT UNSIGNED, c INT UNSIGNED, d INT UNSIGNED, e INT UNSIGNED, f VARCHAR(150), g VARCHAR(150), h VARCHAR(150), i VARCHAR(150) );\n\n";
-print STDERR "LOAD DATA INFILE '/tmp/1.txt' INTO TABLE mytable ( b, c, d, e, f, g, h, i );\n\n";
+print STDERR "CREATE TABLE `sbtest` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `k` int(10) unsigned NOT NULL DEFAULT '0',
+  `c` char(120) NOT NULL DEFAULT '',
+  `pad` char(60) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `k_1` (`k`)
+);\n\n";
+print STDERR "LOAD DATA INFILE '/tmp/1.txt' INTO TABLE sbtest ( k, c, pad );\n\n";
 
 
 foreach( 1..$num_of_rcpt ){
 
-  my $val1 = int( rand(4000000) );
-  my $val2 = int( rand(4000000) );
-  my $val3 = int( rand(4000000) );
-  my $val4 = int( rand(4000000) );
+  my $val1 = int( rand(404000000) );
 
-  my $val5 = generate_random_string( int( rand(95) ) + 5 );
-  my $val6 = generate_random_string( int( rand(95) ) + 5 );
-  my $val7 = generate_random_string( int( rand(95) ) + 5 );
-  my $val8 = generate_random_string( int( rand(95) ) + 5 );
-
-  my $value = "$val1\t$val2\t$val3\t$val4\t$val5\t$val6\t$val7\t$val8\n";
-
-
+  my $value = "$val1\t00000000000-00000000000-00000000000-00000000000-00000000000-00000000000-00000000000-00000000000-00000000000-00000000000\t00000000000-00000000000-00000000000-00000000000-00000000000\n";
   print( $value );
 
 }
-
-
-sub generate_random_string {
-  # http://th.atguy.com/mycode/generate_random_string/
-  my $length_of_randomstring=shift;# the length of 
-       # the random string to generate
-
-  my @chars=('a'..'z');
-  my $random_string;
-  foreach (1..$length_of_randomstring) 
-  {
-    # rand @chars will generate a random 
-    # number between 0 and scalar @chars
-    $random_string.=$chars[rand @chars];
-  }
-  return $random_string;
-}
-
 
