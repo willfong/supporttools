29f3d8647d92dda94864cc81457c35235203f315
Will Fong <w@r10e.xyz>
2016-12-22 14:45:41 +0800

    Adding Ubuntu Sysbench

---
+++ install/ubuntu/sysbench.sh
@@ -0,0 +1,19 @@
+apt-get install -y git libmysqlclient-dev automake libtool make 
+git clone https://github.com/akopytov/sysbench.git
+cd sysbench
+./autogen.sh
+./configure
+make
+
+cd
+
+echo "
+
+
+Sample Sysbench Command:
+
+sysbench/sysbench/sysbench --test=sysbench/sysbench/tests/db/oltp.lua --mysql-user=root --mysql-password= --mysql-db=test --mysql-table-engine=innodb --mysql-ignore-duplicates=on --oltp-read-only=off --oltp-dist-type=uniform --oltp-skip-trx=off --init-rng=on --oltp-test-mode=complex --max-requests=0 --report-interval=30 --num-threads=8 --mysql-host=127.0.0.1 --mysql-port=3306 --oltp-table-size=1000000 --oltp-tables-count=4 --max-time=600 {run|prepare|cleanup}
+
+5M Rows = 1.2G
+"
+
