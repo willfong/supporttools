90263ebb9745f366a019e0cb9d2973b34db1dd5b
Will Fong <w@r10e.xyz>
2017-02-14 14:00:23 +0700

    Typo for echo

--- install/el7/sysbench.sh
+++ install/el7/sysbench.sh
@@ -15,5 +15,5 @@ echo "
 
 Sample Sysbench Command: 
 
-sysbench /usr/local/share/sysbench/oltp_read_write.lua --mysql-user=root --mysql-password= --mysql-db=test --max-requests=0 --report-interval=30 --threads=8 --mysql-host=127.0.0.1 --mysql-port=3306 --table-size=20000000 --tables=1 --time=600 {prepare|run|cleanup}
-
+sysbench /usr/local/share/sysbench/oltp_read_write.lua --mysql-user=root --mysql-password= --mysql-db=test --max-requests=0 --report-interval=30 --threads=8 --mysql-host=127.0.0.1 --mysql-port=3306 --table-size=5000000 --tables=1 --time=600 {prepare|run|cleanup}
+"
