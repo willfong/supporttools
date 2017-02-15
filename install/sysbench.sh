2474e9dfb95b7e25167666fbbc69b14bd2d35706
Will Fong <w@r10e.xyz>
2017-02-15 14:13:46 +0700

    Updated for the new sysbench version

--- install/sysbench.sh
+++ install/sysbench.sh
@@ -4,6 +4,7 @@ cd sysbench
 ./autogen.sh
 ./configure
 make
+make install
 
 cd
 
@@ -12,7 +13,7 @@ echo "
 
 Sample Sysbench Command:
 
-sysbench/sysbench/sysbench --test=sysbench/sysbench/tests/db/oltp.lua --mysql-user=root --mysql-password= --mysql-db=test --mysql-table-engine=innodb --mysql-ignore-duplicates=on --oltp-read-only=off --oltp-dist-type=uniform --oltp-skip-trx=off --init-rng=on --oltp-test-mode=complex --max-requests=0 --report-interval=30 --num-threads=8 --mysql-host=127.0.0.1 --mysql-port=3306 --oltp-table-size=1000000 --oltp-tables-count=4 --max-time=600 {run|prepare|cleanup}
+sysbench /usr/local/share/sysbench/oltp_read_write.lua --mysql-user=root --mysql-password= --mysql-db=test --max-requests=0 --report-interval=30 --threads=8 --mysql-host=127.0.0.1 --mysql-port=3306 --table-size=5000000 --tables=1 --time=600
 
 5M Rows = 1.2G
 "
