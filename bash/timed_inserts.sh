b239732d82e5ff24d60796f4ef887771bb93657f
Will Fong <w@r10e.xyz>
2017-03-03 12:34:53 +0800

    Adding more tests

--- bash/timed_inserts.sh
+++ bash/timed_inserts.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 TIMEFORMAT=%R
 
+
+mysql test -e "DROP TABLE IF EXISTS timed_inserts"
+mysql test -e "CREATE TABLE timed_inserts ( a INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, b DATETIME, c INT UNSIGNED)"
+
+
+trap 'break' INT
+
+NUMBER=0
+
 while true
 do
-  echo -n `date +"%T.%3N"` --\  
-  time mysql test -e "INSERT INTO test (b) VALUES (NOW())"
+  NUMBER=$((NUMBER+1))
+  echo -n `date +"%T.%3N"` --\ 
+  echo -n "# $NUMBER  Execution time: "
+  time mysql test -e "INSERT INTO timed_inserts (b, c) VALUES (NOW(), $NUMBER)"
   sleep 1
 done
 
+
+mysql test -e "SELECT COUNT(*) FROM timed_inserts"
