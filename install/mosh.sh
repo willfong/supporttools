94d3d613ee7415114ed095ac4500a73e4335e16f
Will Fong <w@r10e.xyz>
2017-03-06 13:06:58 +0800

    Version bump

--- install/mosh.sh
+++ install/mosh.sh
@@ -1,2 +1,2 @@
-yum -y install https://dl.fedoraproject.org/pub/epel/6/x86_64/mosh-1.2.6-1.el6.x86_64.rpm http://dl.fedoraproject.org/pub/epel/6/x86_64/protobuf-2.3.0-9.el6.x86_64.rpm 
+yum -y install https://dl.fedoraproject.org/pub/epel/6/x86_64/mosh-1.2.6-2.el6.x86_64.rpm http://dl.fedoraproject.org/pub/epel/6/x86_64/protobuf-2.3.0-9.el6.x86_64.rpm 
 
