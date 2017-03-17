a57125493445fd428ac6f43d7d559dd946abf631
Will Fong <w@r10e.xyz>
2017-03-17 20:53:20 +0800

    Deps

--- install/el7/mosh.sh
+++ install/el7/mosh.sh
@@ -1,2 +1,2 @@
-yum -y install https://dl.fedoraproject.org/pub/epel/7/x86_64/m/mosh-1.2.6-1.el7.x86_64.rpm 
+yum -y install https://dl.fedoraproject.org/pub/epel/7/x86_64/m/mosh-1.2.6-1.el7.x86_64.rpm  https://dl.fedoraproject.org/pub/epel/7/x86_64/p/protobuf-2.5.0-7.el7.x86_64.rpm
 
